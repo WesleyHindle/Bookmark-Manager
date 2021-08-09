4da9d5494605fb7812ca1fff1b96526ac45a3f22
Michael Poole <michaelpoole10@gmail.com>
2021-08-09 14:36:57 +0100

    file structure setup completed (hopefully)

---
+++ app.rb
@@ -0,0 +1,7 @@
+
+require 'sinatra/base'
+
+class Bookmarks < Sinatra::Base
+  
+  run! if app_file == $0
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,12 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'capybara/rspec'
+
+ENV['RACK_ENV'] = 'test'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+Capybara.app = Bookmarks
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
