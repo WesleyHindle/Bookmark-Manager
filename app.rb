27634e2993ccf8fcde9d8b6b78d302146bec05ff
Michael Poole <michaelpoole10@gmail.com>
2021-08-09 15:17:37 +0100

    fixed a small issue with the gemfile

--- app.rb
+++ app.rb
@@ -5,14 +5,14 @@ class Bookmarks < Sinatra::Base
   configure :development do 
     register Sinatra::Reloader 
   end
-  
-get "/test" do
-  "test page"
-end 
+    
+  get "/test" do
+    "test page"
+  end 
 
-get "/" do
-  erb :index
-end
+  get "/" do
+    erb :index
+  end
 
   run! if app_file == $0
 end
